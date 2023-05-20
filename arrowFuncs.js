421ca736bd876248f042348db07e9caec83a3cb4
Muhammad Masood <yoursourcecode@gmail.com>
2023-05-20 10:16:49 -0700

    new_commit

---
+++ arrowFuncs.js
@@ -0,0 +1,39 @@
+//if you want data which is outside the scope/function/object so use arrow funcs
+//example of hardisk, one folder will be present in one hardisk.
+//Arrow func doesn't create its own scope, it is connected to the main function already.
+
+
+// const add = (x=4,y=6) =>{
+//     return x+y;
+// }
+
+// console.log(add(45,67));
+
+// const obj =  {
+//     prop1: "Masood"
+// }
+
+// const {prop1} = obj;
+// console.log(prop1);
+
+// const arr = ["Masood","Waheed"];
+// const [fName,lName] = arr;
+// console.log(fName,lName);
+
+// const restArray = ["abc","def","ghy"];
+// const newArray = ["aaa", ...restArray,"xyz"];
+// console.log(newArray);
+
+// const obj1 = {
+//     message: "Hello"
+// }
+
+// const obj2 = {
+//     ...obj1,
+//     name:"Masood",
+//     id: 37
+// }
+
+// console.log(obj2);
+
+5
